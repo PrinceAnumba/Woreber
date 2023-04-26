fa4963cd276b220f11bb2886a135ad79daea98c4
Prince <princeanumba0@gmail.com>
2023-04-26 11:04:02 +0000

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='font-poppins'>
+       <Header/>
+    </div>
+  )
+}
+
+export default App
